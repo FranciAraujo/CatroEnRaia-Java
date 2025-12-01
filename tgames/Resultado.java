a196f1d99f42b3e722aeb5e65d3743541b53384d
Franci Araujo <107490412+FranciAraujo@users.noreply.github.com>
2025-12-01 02:38:04 +0100

    Create Resultado.java
    Esta clase simple se utiliza para encapsular el mensaje final del juego (victoria o empate) y es utilizada por el Motor y el Xogo (Juego).
    
    Añadida clase Resultado para manejar el fin del juego

---
+++ tgames/Resultado.java
@@ -0,0 +1,7 @@
+package tgames;
+public class Resultado {
+    private String mensaje;
+    public Resultado(String mensaje) { this.mensaje = mensaje; }
+    @Override
+    public String toString() { return mensaje; }
+}

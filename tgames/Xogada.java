37e99f69abf95967ea5c08d0af9da2c2fcfcb5b7
Franci Araujo <107490412+FranciAraujo@users.noreply.github.com>
2025-12-01 02:42:43 +0100

    Create Xogada.java
    Esta clase base representa el concepto de movimiento o jugada realizado por un Xogador. Las jugadas específicas para el 4 en Raya (que incluirán la columna) heredarán de esta.
    
    Añadida clase base Xogada

---
+++ tgames/Xogada.java
@@ -0,0 +1,6 @@
+package tgames;
+public class Xogada {
+    protected Xogador xogador; 
+    public Xogada(Xogador xogador) { this.xogador=xogador; }
+    public Xogador getXogador() { return xogador; }
+}

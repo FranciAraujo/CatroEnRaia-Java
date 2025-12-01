59a8a9722a24218750684a51b17c57b18d7907f6
Franci Araujo <107490412+FranciAraujo@users.noreply.github.com>
2025-12-01 02:34:20 +0100

    Create Ficha.java
    Inicia paquete tgames con la clase Ficha

---
+++ tgames/Ficha.java
@@ -0,0 +1,7 @@
+package tgames;
+public class Ficha {
+    private String representacion;
+    public Ficha(String representacion) { this.representacion = representacion; }
+    @Override
+    public String toString() { return representacion; }
+}
